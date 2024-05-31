bfacd48878da00cf3f19f7c20bbcbba2a92a05c3
Matheus Müller <matheussmuller@icloud.com>
2024-05-31 11:59:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from tkinter import *
+from tkinter import ttk
 
 
 window = Tk()
 window.title('Employee Registration')
-window.geometry('700x600')
+window.geometry('1200x700')
 # window.config(bg='white')
 
 frame = Frame(window)
 frame.pack()
 
+# Primeira parte
 user_info_frame = LabelFrame(frame, text='User Info')
-user_info_frame.grid(row=0, column=0, padx=20, pady=20, sticky=W)
+user_info_frame.grid(row=0, column=0, padx=225, pady=20, sticky=W)
 
+# Pegar informacoes do funcionario
 first_name = Label(user_info_frame, text='Nome :')
 first_name.grid(row=0, column=0, padx=15, pady=10, sticky=W)
 
-# Pegar informacoes do funcionario
 first_name = Entry(user_info_frame)
 first_name.grid(row=0, column=1, padx=15, pady=10, sticky=W)
 
@@ -31,11 +33,11 @@ ident.grid(row=1, column=0, padx=15, pady=10, sticky=W)
 ident = Entry(user_info_frame)
 ident.grid(row=1, column=1, padx=15, pady=10, sticky=W)
 
-teste = Label(user_info_frame, text='teste :')
-teste.grid(row=1, column=2, padx=5, pady=10, sticky=W)
+role = Label(user_info_frame, text='Funcao :')
+role.grid(row=1, column=2, padx=5, pady=10, sticky=W)
 
-teste = Entry(user_info_frame)
-teste.grid(row=1, column=3, padx=15, pady=10, sticky=W)
+role = Entry(user_info_frame)
+role.grid(row=1, column=3, padx=15, pady=10, sticky=W)
 
 # Botao para cada opcao
 clean_bt = Button(user_info_frame, text='Apagar informacoes')
@@ -49,5 +51,12 @@ delete_bt.grid(row=2, column=2, padx=15, pady=10)
 
 search_bt = Button(user_info_frame, text='Apagar informacoes')
 search_bt.grid(row=2, column=3, padx=15, pady=10)
+
+# Segunda parte
+show_info_frame = LabelFrame(frame, text='View users information')
+show_info_frame.grid(row=1, column=0, padx=5, pady=5, sticky=W)
+
+client_list = ttk.Treeview(show_info_frame, columns=('FirstName', 'Age', 'Ident', 'Role'))
+client_list.grid(row=0, column=0, padx=10, columnspan=1, pady=10, sticky=W)
 
 window.mainloop()
